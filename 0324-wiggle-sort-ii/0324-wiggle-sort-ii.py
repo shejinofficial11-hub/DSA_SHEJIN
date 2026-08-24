c2dd44f051d4deacaea70a11ebac683f4066d235
shejinofficial11-hub <shejinofficial11@gmail.com>
2026-08-24 10:56:21 +0530

    Time: 7 ms (66.91%), Space: 20.6 MB (44.69%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -1,17 +1,22 @@
 class Solution(object):
     def wiggleSort(self, nums):
+
         nums.sort()
         n = len(nums)
         result = []
         left = (n-1)//2
         right  = n-1
+
         for i in range(n):
+
             if i % 2 == 0:
+
                 result.append(nums[left])
                 left -= 1
+
             else:
+
                 result.append(nums[right])
                 right -= 1
 
-        for i in range(n):
-            nums[i] = result[i]
+        nums[:]=result
