4d4b7669d9a67dbc66c6b5fe9733e07b17a0f734
shejinofficial11-hub <shejinofficial11@gmail.com>
2026-08-31 11:37:38 +0530

    Time: 40 ms (91.71%), Space: 20.1 MB (19.73%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def copyRandomList(self, head):
+        if not head:
+            return None
+
+        old_to_new = {}
+
+        current = head
+
+        while current:
+            old_to_new[current] = Node(current.val)
+            current = current.next
+
+        current = head
+
+        while current:
+            old_to_new[current].next = old_to_new.get(current.next)
+            old_to_new[current].random = old_to_new.get(current.random)
+            current = current.next
+
+        return old_to_new[head]
