1b9bc3c653ae6b3a322e4ba27f577ec903a4b85f
shejinofficial11-hub <shejinofficial11@gmail.com>
2026-09-03 09:18:51 +0530

    Time: 79 ms (86.26%), Space: 31.9 MB (53.48%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val):
+        self.stack.append(val)
+
+        if len(self.minStack) == 0:
+            self.minStack.append(val)
+        else:
+            if val < self.minStack[-1]:
+                self.minStack.append(val)
+            else:
+                self.minStack.append(self.minStack[-1])
+
+    def pop(self):
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.minStack[-1]
